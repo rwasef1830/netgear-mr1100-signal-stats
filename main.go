760cd0a5f5ccf79cedf7fb7d0633ae7f6e2543c0
Raif Atef <beliskner.github.3psil0n@neverbox.com>
2025-07-27 16:08:06 +0300

    2nd attempt

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"go.oneofone.dev/gserv"
 )
+
+const pipePath = "/dev/smd7"
+
+var resultChan = make(chan string, 1)
 
 func main() {
 	// Define a variable to hold the port number
@@ -30,6 +37,8 @@ func main() {
 }
 
 func start(port int) {
+	go consumeCommandResults()
+
 	srv := gserv.New()
 
 	svc := Svc{}
@@ -43,6 +52,7 @@ type Svc struct {
 	// some stuff here
 }
 
+//goland:noinspection SpellCheckingInspection
 func (s *Svc) HandleRequest(*gserv.Context) gserv.Response {
 	html := `
 <!DOCTYPE html>
@@ -58,7 +68,7 @@ func (s *Svc) HandleRequest(*gserv.Context) gserv.Response {
 	finalOutput := ""
 
 	var commandOutputs []string
-	commandOutputs = append(commandOutputs, execAtCommandAndGetResponse("AT!GTATUS?"))
+	commandOutputs = append(commandOutputs, execAtCommandAndGetResponse("AT!GSTATUS?"))
 
 	finalOutput = strings.Join(commandOutputs, "\n<br />\n")
 	html = strings.Replace(html, "[placeholder]", finalOutput, 1)
@@ -67,7 +77,14 @@ func (s *Svc) HandleRequest(*gserv.Context) gserv.Response {
 }
 
 func execAtCommandAndGetResponse(command string) string {
-	return execCommandAndGetResponse("sh", "-c", fmt.Sprintf("\"echo -e '\r\n%s\r\n' > /dev/smd7 && cat /dev/smd7\"", command))
+	_ = execCommandAndGetResponse("sh", "-c", fmt.Sprintf("echo -e \"\r\n%s\r\n\" > %s", command, pipePath))
+	select {
+	case result := <-resultChan:
+		return result
+
+	case <-time.After(1 * time.Second):
+		return "<timed out reading response>"
+	}
 }
 
 func execCommandAndGetResponse(command string, args ...string) string {
@@ -85,4 +102,48 @@ func execCommandAndGetResponse(command string, args ...string) string {
 		output = strings.Trim(output, "\r\n \t")
 		return "<pre>" + output + "</pre>"
 	}
+}
+
+func consumeCommandResults() {
+	file, err := os.Open(pipePath)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		close(resultChan)
+		return
+	}
+
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	buffer := ""
+	for scanner.Scan() {
+		line := scanner.Text()
+		buffer += line + "\n"
+		if line == "OK" {
+			select {
+			case resultChan <- buffer:
+				buffer = ""
+
+			default:
+				resultChan <- buffer
+				buffer = ""
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+	}
+
+	if buffer != "" {
+		select {
+		case resultChan <- buffer:
+		default:
+			resultChan <- buffer
+		}
+	}
+
+	close(resultChan)
 }
